e700c4cad4e3c246dbcbeb19025e2fcab7095f8c
Koen3740 <koen.munsters@hotmail.com>
2026-06-19 23:46:58 +0200

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="tradetracker-site-verification"
+        content="b07ba393d880b3521ed1b2ff19fc9aa425113cfa"
+      />
+    </>
+  );
+}
